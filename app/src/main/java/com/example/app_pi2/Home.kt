edf506adbf03e8d4219b39f5a2dbd36d474481ea
Mariannagb <marianna.mgb@gmail.com>
2025-10-27 21:18:23 -0300

    Novas atualizações

--- app/src/main/java/com/example/app_pi2/Home.kt
+++ app/src/main/java/com/example/app_pi2/Home.kt
@@ -32,13 +32,9 @@ class Home : AppCompatActivity() {
 
         adapter = InteracaoAdapter(interacoesList) { position ->
             val interacao = interacoesList[position]
-            val intent = Intent(this, DetalheInteracao::class.java).apply {
-                putExtra("TITULO", interacao.titulo)
-                putExtra("DESCRICAO", interacao.descricao)
-                putExtra("IMAGEM_URL", interacao.imagem)
-            }
-            startActivity(intent)
+
         }
+        
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = adapter
 
